0791a4b44276bf318d2dfad226bff0b8a6cb684e
marprezd <perezdhoz@gmail.com>
2023-03-02 22:21:56 -0500

    Add top navigation item.

---
+++ src/data/topNavItems.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    name: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 3,
+    name: "Projects",
+    url: "/projects",
+  },
+  {
+    id: 4,
+    name: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    url: "/contact",
+  },
+]
+
+export default data
