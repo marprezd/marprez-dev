8846ba72ed02a74a54955ba7fa922977ae56ff81
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2023-05-08 15:28:23 -0500

    Fix ReferenceError: es5 is not defined

--- prettier.config.js
+++ prettier.config.js
@@ -5,7 +5,7 @@ module.exports = {
     semi: false,
     singleQuote: true,
     printWidth: 100,
-    trailingComma: es5,
+    trailingComma: 'es5',
     bracketSpacing: true,
     // plugins: [require('prettier-plugin-tailwindcss')],
   }
