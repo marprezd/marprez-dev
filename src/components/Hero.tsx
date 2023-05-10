b80e769a48be0c147a31a24d0fe8022e67d740da
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2023-05-10 09:34:18 -0500

    Add Hero component.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import DevActivitySvg from './DevActivitySvg'
+
+export const Hero: React.FC = () => {
+  return (
+    <div className="grid grid-cols-12 gap-4">
+      <div className="order-last col-span-full place-self-center text-center sm:text-left md:order-first md:col-span-6">
+        <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white lg:text-5xl">
+          Hello, <span className="text-secondary-600 dark:text-secondary-500">I am Mario!</span>{' '}
+          Welcome to my <span className="text-secondary-600 dark:text-secondary-500">personal</span>{' '}
+          website.
+        </h1>
+        <p className="my-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
+          I write about Python, Machine Learning, Data Visualization, APIs Design, UI App, Java, and
+          other Tech subjects.
+        </p>
+      </div>
+      <div className="col-span-full md:col-span-6">
+        <DevActivitySvg />
+      </div>
+    </div>
+  )
+}
