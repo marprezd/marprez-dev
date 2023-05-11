8c27c0d3300685669e265494c55e6048be9858d0
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2023-05-10 20:44:57 -0500

    Fix: Module not found: Can’t resolve ‘fs’. Update images domains.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'cdn.buymeacoffee.com'],
+    domains: ['images.unsplash.com', 'cdn.buymeacoffee.com', 'res.cloudinary.com'],
   },
   reactStrictMode: true,
   swcMinify: true,
@@ -18,6 +18,16 @@ const nextConfig = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      }
+    }
+
+    return config
   },
 }
 
