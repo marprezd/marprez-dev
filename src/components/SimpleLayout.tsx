d7afee032f0782b967697fb3e43f62457b196370
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2023-05-21 10:47:55 -0500

    Minor changes: Update header element margins.

--- src/components/SimpleLayout.tsx
+++ src/components/SimpleLayout.tsx
@@ -12,10 +12,10 @@ export default function SimpleLayout({
   return (
     <section className="bg-gradient-to-b from-gray-200 to-gray-50 py-10 dark:from-gray-950 dark:to-gray-900 lg:py-20">
       <Container>
-        <header className="relative z-10 mx-auto max-w-full px-4 text-center">
+        <header className="relative my-4 z-10 mx-auto max-w-full px-4 text-center">
           <h1 className="text-4xl font-bold leading-none tracking-tight lg:text-5xl">{title}</h1>
           {intro && (
-            <h2 className="my-4 text-lg font-normal sm:px-16 lg:px-48 lg:text-xl">{intro}</h2>
+            <h2 className="text-lg font-normal sm:px-16 lg:px-48 lg:text-xl">{intro}</h2>
           )}
         </header>
         <div>{children}</div>
